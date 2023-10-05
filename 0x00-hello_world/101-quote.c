7ab7b2c870adaeab18b95eb98dbd4ecf9c14300c
Oluwatobiloba Olajide <mroluwatobilobaezekiel@gmail.com>
2023-10-05 23:20:33 +0100

    used the write funct for simple output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - output given string to the standard error.
+ * this works by using the write function that takes 2 as value 
+ * Return: 0 on Success
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
