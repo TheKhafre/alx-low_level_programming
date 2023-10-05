314e7f5791c3a5b632c786cb3b454591f70e71a2
Oluwatobiloba Olajide <mroluwatobilobaezekiel@gmail.com>
2023-10-05 23:16:44 +0100

    attempted mandatory introductory c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the program outputs a give phrase 
+ * followed by newline.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
