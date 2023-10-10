57fc59549e83ea124b551a3e0d59150c532f3998
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2023-10-10 16:00:47 +0100

    corrected betty style error

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,16 +10,8 @@ int main(void)
 	char pp[] = "_putchar";
 	int i = 0;
 
-	/*while (pp[i] != '\0')
-	{
-		_putchar(pp[i]);
-		i++;
-	}
-	_putchar('\n');*/
-
 	for (i = 0; pp[i] != '\0'; i++)
 	{
-		/* code */
 		_putchar(pp[i]);
 	}
 
