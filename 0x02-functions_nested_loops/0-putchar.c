e33634d157b74db0a28d9db8000277f359eb0e52
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2023-10-10 15:43:02 +0100

    a simple program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * main - Print "_putchar" followed by a new line.
+ * Instruction: the standard libraries must be avoided.
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+	char pp[] = "_putchar";
+	int i = 0;
+
+	/*while (pp[i] != '\0')
+	{
+		_putchar(pp[i]);
+		i++;
+	}
+	_putchar('\n');*/
+
+	for (i = 0; pp[i] != '\0'; i++)
+	{
+		/* code */
+		_putchar(pp[i]);
+	}
+
+	_putchar('\n');
+	
+	return (0);
+}
